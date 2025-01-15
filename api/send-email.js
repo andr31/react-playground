65a3c76d6b64ea238c60bacb17c248c1cc61f3f3
Andrei Luchian <andreiluchian@Ingas-MacBook-Air.local>
2025-01-15 14:30:45 -0700

    update imnports in send email function

--- api/send-email.js
+++ api/send-email.js
@@ -1,4 +1,5 @@
-const sgMail = require('@sendgrid/mail');
+// api/send-email.js
+import sgMail from '@sendgrid/mail';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
