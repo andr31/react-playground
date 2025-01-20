65e3a3b49ea0ecbb96e6ba6583f33bf8f39285a8
Andrei Luchian <luchian@blackduck.com>
2025-01-20 16:41:29 -0700

    update about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 const AboutMe: React.FC = () => {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-center bg-[#dbccc3] p-5">
-      <div className="">
-        <img
-          src="/static/images/Cristina.jpg"
-          alt="About Me"
-          className="w-60 h-82 lg:w-[500px] lg:h-auto rounded-lg"
-        />
-      </div>
-      <div className="pt-5 lg:pt-0 max-w-2xl lg:ml-5">
-        <p className="text-lg text-center">
+    <div className="flex flex-collg:flex-row items-center justify-center bg-[#dbccc3] p-5">
+      <img
+        src="/static/images/Cristina.jpg"
+        alt="About Me"
+        className="w-60 h-82 lg:w-[500px] lg:h-auto rounded-lg"
+      />
+      <div className="prose pl-20">
+        <h1>I am Cristina</h1>
+        <p className="text-lg text-center max-w-lg">
           I am a photographer, a storyteller, an adventurer, and a lover of
           golden sunsets and ocean breezes. I’m a wife to my biggest supporter
           and a mom to two wonderful kids who keep me inspired every day. I’m
